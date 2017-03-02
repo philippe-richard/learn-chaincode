7c7e937a460c4ef04a6a1b136ee2b99c2e0df7ec
philippe_richard <philippe_richard@fr.ibm.com>
2017-03-02 17:12:27 +0100

    new comment:

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -1,6 +1,6 @@
 /*
 
-modif philippe 2 mars 2017
+modif2 philippe 2 mars 2017
 Copyright IBM Corp 2016 All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +14,9 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+
+
 */
 
 package main
